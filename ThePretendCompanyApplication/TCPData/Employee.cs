06527af6c73d7b5ca4b635c16d8e4c5fbef2102b
Sonu Thapa <37073011+mesailesh7@users.noreply.github.com>
2025-05-18 17:47:33 -0600

    Linq Project

---
+++ ThePretendCompanyApplication/TCPData/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace TCPData
+{
+    public class Employee
+    {
+ 
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public decimal AnnualSalary { get; set; }
+        public bool IsManager { get; set; }
+        public int DepartmentId{ get; set; }
+
+    }
+}
+
---
+++ ThePretendCompanyApplication/TCPExtensions/Extension.cs
@@ -0,0 +1,6 @@
+namespace TCPExtensions;
+
+public static class Extension
+{
+    public static List<T> Filter<T>(this List<T> records, func)
+}
