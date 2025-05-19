e9a7199d064869614f872a28c4b8ab105cae0900
Sonu Thapa <37073011+mesailesh7@users.noreply.github.com>
2025-05-19 00:41:39 -0600

    List practise

---
+++ List/List/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace List
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> shoppingList = new List<string>
+            {
+                "chocolate",
+                "banana",
+                "sugar",
+                "milk",
+                "cereal"
+            };
+
+            shoppingList.Add("Lemon");
+            shoppingList.Add("Apple");
+
+            foreach (string shop in shoppingList)
+            {
+                Console.WriteLine(shop);
+            }
+        }
+    }
+}
