0c2310e024411841e664841da756255eece92a05
Sonu Thapa <37073011+mesailesh7@users.noreply.github.com>
2025-07-13 11:47:39 -0600

    .Net api Update

--- Reactivities/API/Controllers/ActivitiesController.cs
+++ Reactivities/API/Controllers/ActivitiesController.cs
@@ -1,14 +1,35 @@
 using System;
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Persistance;
 
 namespace API.Controllers;
 
-public class ActivitiesController : BaseApiController
+//The way it is doing right now is using private constructor
+public class ActivitiesController(AppDbContext context) : BaseApiController
 {
-    private readonly AppDbContext context;
-    public ActivitiesController(AppDbContext context)
+    //This is the old of way doing
+    // private readonly AppDbContext context;
+    // public ActivitiesController(AppDbContext context)
+    // {
+    //     this.context = context;
+    // }
+
+    [HttpGet]
+    public async Task<ActionResult<List<Activity>>> GetActivities()
     {
-        this.context = context
+        return await context.Activities.ToListAsync();
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Activity>> GetActivityDetail(string id)
+    {
+        var activity = await context.Activities.FindAsync(id);
+
+        if (activity == null) return NotFound();
+
+        return activity;
     }
 
 }
