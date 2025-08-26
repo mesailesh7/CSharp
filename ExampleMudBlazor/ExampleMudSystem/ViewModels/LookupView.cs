c797507b5aa961c9cec6533f0cb20e0ade272cad
Sonu Thapa <37073011+mesailesh7@users.noreply.github.com>
2025-08-26 14:00:15 -0600

    Create LookupView.cs

---
+++ ExampleMudBlazor/ExampleMudSystem/ViewModels/LookupView.cs
@@ -0,0 +1,9 @@
+﻿namespace ExampleMudSystem.ViewModels;
+
+public class LookupView
+{
+    public int LookupID { get; set; }
+    public int CategoryID {get; set;}
+    public string Name { get; set; }
+    public bool RemoveFromViewFlag { get; set; }
+}
