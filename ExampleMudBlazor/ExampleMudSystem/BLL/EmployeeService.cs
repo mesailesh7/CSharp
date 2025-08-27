2fc80deb70437294465533710b575c2d83eb2859
Sonu Thapa <37073011+mesailesh7@users.noreply.github.com>
2025-08-27 13:30:05 -0600

    EmployeeFullName Function

---
+++ ExampleMudBlazor/ExampleMudSystem/BLL/EmployeeService.cs
@@ -0,0 +1,23 @@
+﻿using ExampleMudSystem.DAL;
+
+namespace ExampleMudSystem.BLL;
+
+public class EmployeeService
+{
+    private readonly OLTPDMIT2018Context _hogWildContext;
+
+    internal EmployeeService(OLTPDMIT2018Context hogWildContext)
+    {
+        _hogWildContext = hogWildContext;
+    }
+    
+    public string GetEmployeeFullName(int employeeID)
+    {
+        return _hogWildContext.Employees
+                   .Where(x => x.EmployeeID == employeeID
+                               && !x.RemoveFromViewFlag)
+                   .Select(x => $"{x.FirstName} {x.LastName}")
+                   .FirstOrDefault()
+               ?? string.Empty;
+    }
+}
