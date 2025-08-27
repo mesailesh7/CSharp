ce97566179c65662cc2b83c3b4b0461d976e5d4c
Sonu Thapa <37073011+mesailesh7@users.noreply.github.com>
2025-08-27 13:30:25 -0600

    Invoice and Part View Creation

---
+++ ExampleMudBlazor/ExampleMudSystem/ViewModels/InvoiceView.cs
@@ -0,0 +1,16 @@
+﻿namespace ExampleMudSystem.ViewModels;
+
+public class InvoiceView
+{
+    public int InvoiceID { get; set; }
+    public DateOnly InvoiceDate { get; set; }
+    public int CustomerID { get; set; }
+    public string CustomerName { get; set; }
+    public int EmployeeID { get; set; }
+    public string EmployeeName { get; set; }
+    public decimal SubTotal { get; set; }
+    public decimal Tax { get; set; }
+    public decimal Total => SubTotal + Tax;
+    public List<InvoiceLineView> InvoiceLines { get; set; } = new List<InvoiceLineView>();
+    public bool RemoveFromViewFlag { get; set; }
+}
---
+++ ExampleMudBlazor/ExampleMudSystem/ViewModels/PartView.cs
@@ -0,0 +1,14 @@
+﻿namespace ExampleMudSystem.ViewModels;
+
+public class PartView
+{
+    public int PartID { get; set; }
+    public int PartCategoryID { get; set; }
+    public string CategoryName { get; set; }
+    public decimal Cost { get; set; }
+    public decimal Price { get; set; }
+    public int ROL { get; set; }
+    public int QOH { get; set; }
+    public bool Taxable { get; set; }
+    public bool RemoveFromViewFlag { get; set; }
+}
