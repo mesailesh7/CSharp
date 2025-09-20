968529e4afffcdae48b2c048c427c26063bdb4ec
Sonu Thapa <37073011+mesailesh7@users.noreply.github.com>
2025-09-20 12:28:16 -0600

    database connectionString defined

--- DotnetReactCrud/Backend/Backend/Program.cs
+++ DotnetReactCrud/Backend/Backend/Program.cs
@@ -1,8 +1,15 @@
+using Backend.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 //services
 // Controller based api
 builder.Services.AddControllers();
 
+//Connection string can be defined like this as well but its not a good pracitse so we define connection string in appsettings.json and pull it from there
+// string connectionString = "Data Source = Person.db";
+string connectionString = builder.Configuration.GetConnectionString("Default") ?? throw new ArgumentException("Connection string is null");
+builder.Services.AddDbContext<AppDbContext>(op => op.UseSqlite(""));
 
 var app = builder.Build();
 
