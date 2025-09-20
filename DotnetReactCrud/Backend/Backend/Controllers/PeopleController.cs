7a5051d45d33dababa28ae716738553ddb797856
Sonu Thapa <37073011+mesailesh7@users.noreply.github.com>
2025-09-20 13:51:26 -0600

    api: Post, get, put, delete

--- DotnetReactCrud/Backend/Backend/Controllers/PeopleController.cs
+++ DotnetReactCrud/Backend/Backend/Controllers/PeopleController.cs
@@ -35,7 +35,22 @@ namespace Backend.Controllers
                 _context.People.Add(person);
                 //saving the changes to the database
                 await _context.SaveChangesAsync();
-                return Ok(person); // this will return 200 ok status code + person object in the body
+                return CreatedAtRoute("GetPerson", new { id = person.Id }, person); // this will return 201 (Created status code) ok status code + person object in the body ** 201 created status code + location of the resource (http://localhost:3000/api/people/{id}) + person object in the response body
+
+                // Example response from the api
+                //                 HTTP/1.1 201 Created
+                // Connection: close
+                // Content-Type: application/json; charset=utf-8
+                // Date: Sat, 20 Sep 2025 19:27:49 GMT
+                // Server: Kestrel
+                // Location: http://localhost:3000/api/People/3
+                // Transfer-Encoding: chunked
+
+                // {
+                //   "id": 3,
+                //   "firstName": "Sun",
+                //   "lastName": "T"
+                // }
 
             }
             catch (Exception ex)
@@ -46,7 +61,7 @@ namespace Backend.Controllers
         }
 
         [HttpGet] //GET /api/people
-        public async Task<IActionResult> GetPeople(Person person)
+        public async Task<IActionResult> GetPeople()
         {
             try
             {
@@ -54,7 +69,36 @@ namespace Backend.Controllers
                 var people = await _context.People.ToListAsync();
                 //if we want to select only certain people
                 // var people = await _context.People.select(c => new {
-                // c.id, c.firstName, c.lastName
+                // c.id, c.firstName,
+                // }).ToListAsync();
+
+                //saving the changes to the database
+                return Ok(people); // this will return 200 ok status code + person object in the body
+
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+           ; // 500 internal server error + message in the body response body
+        }
+
+
+        [HttpGet("{id:int}", Name = "GetPerson")] //GET /api/people/1
+        public async Task<IActionResult> GetPerson(int id)
+        {
+            try
+            {
+                // At this points of time data is added to the context(context is like a cached data in the memory) but its not added to the database
+                var person = await _context.People.FindAsync(id);
+
+                if (person is null)
+                {
+                    return NotFound(); // 404 not found status code
+                }
+                //if we want to select only certain people
+                // var people = await _context.People.select(c => new {
+                // c.id, c.firstName,
                 // }).ToListAsync();
 
                 //saving the changes to the database
@@ -65,8 +109,68 @@ namespace Backend.Controllers
             {
                 return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
             }
-           ; // 500 internal server error + message in the body response body
+                ; // 500 internal server error + message in the body response body
         }
+
+
+        [HttpPut("{id:int}")] //Put /api/people/1
+        public async Task<IActionResult> UpdatePerson(int id, [FromBody] Person person)
+        {
+            try
+            {
+
+                if (id != person.Id)
+                {
+                    return BadRequest("Id in url and body mismatches"); //400 + message in body
+                }
+
+                if (!await _context.People.AnyAsync(p => p.Id == id))
+                {
+                    return NotFound();
+                }
+                // At this points of time data is added to the context(context is like a cached data in the memory) but its not added to the database
+                _context.People.Update(person);
+                //saving the changes to the database
+                await _context.SaveChangesAsync();
+                return NoContent(); //204 status code
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+            // 500 internal server error + message in the body response body
+        }
+
+
+        [HttpDelete("{id:int}")] //Delete /api/people/1
+        public async Task<IActionResult> DeletePerson(int id)
+        {
+            try
+            {
+                var person = await _context.People.FindAsync(id);
+
+                if (person is null)
+                {
+                    return NotFound();
+                }
+
+                _context.People.Remove(person);
+
+                // At this points of time data is added to the context(context is like a cached data in the memory) but its not added to the database
+
+                //saving the changes to the database
+                await _context.SaveChangesAsync();
+                return NoContent(); //204 status code
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+            // 500 internal server error + message in the body response body
+        }
+
+
+
     }
 
 
