5d589faa3521788032ea56c7b542b7f6f29d20d1
Sonu Thapa <37073011+mesailesh7@users.noreply.github.com>
2025-09-20 15:01:12 -0600

    routes created and Navbar updated

---
+++ DotnetReactCrud/client/src/App.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Navbar from "./components/NavBar";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import NotFound from "./pages/NotFound";
+import Person from "./components/person/Person";
+import { Routes, Route } from "react-router";
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="Person" element={<Person />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </>
+  );
+};
+
+export default App;
